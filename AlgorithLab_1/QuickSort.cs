8d495f2f08ef7b51868c9752457cf8066396a986
FazylovV <vladislav.fazylov04@gmail.com>
2023-09-21 12:16:25 +0500

    added bubble and quick sorts

---
+++ AlgorithLab_1/QuickSort.cs
@@ -0,0 +1,42 @@
+namespace AlgorythmLab1;
+
+public class QuickSort : ISortable
+{
+    public static int[] Sort(int[] arr)
+    {
+        return SortArray(arr, 0, arr.Length-1);
+    }
+    
+    public static int[] SortArray(int[] array, int leftIndex, int rightIndex)
+    {
+        var i = leftIndex;
+        var j = rightIndex;
+        var pivot = array[leftIndex];
+        while (i <= j)
+        {
+            while (array[i] < pivot)
+            {
+                i++;
+            }
+        
+            while (array[j] > pivot)
+            {
+                j--;
+            }
+            if (i <= j)
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+    
+        if (leftIndex < j)
+            SortArray(array, leftIndex, j);
+        if (i < rightIndex)
+            SortArray(array, i, rightIndex);
+        return array;
+    }
+}
---
+++ AlgorithLab_1/Sort.cs
@@ -0,0 +1,9 @@
+namespace AlgorythmLab1;
+
+interface ISortable
+{
+    public static int[] Sort(int[] arr)
+    {
+        return arr;
+    }
+}
