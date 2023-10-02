decc69f170b4b1907e9f64f777641ee0ccd15673
FazylovV <vladislav.fazylov04@gmail.com>
2023-10-02 21:47:42 +0500

    сделано решето эратосфена и немного сделан рисовщик графиков

--- AlgorithLab_1/Drawer.cs
+++ AlgorithLab_1/Drawer.cs
@@ -1,6 +1,15 @@
+using ScottPlot;
+
 namespace AlgorithLab_1;
 
 public class Drawer
 {
-    
+    public static void Draw(List<double> dataX, List<double> dataY, string name, string pathPNG)
+    {
+        Plot plot = new Plot();
+        plot.Title(name);
+
+        plot.Add.Scatter(dataX, dataY, Colors.Aqua);
+        plot.SavePng(pathPNG, 1000, 1000);
+    }
 }
